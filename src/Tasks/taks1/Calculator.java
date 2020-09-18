8e5a71229ee22b8d9fa7ef28c9d310be22f935fc
Aleksandr_Markov <aleksandr.markovgit@gmail.com>
2020-09-18 19:44:24 +0300

    1

--- src/Tasks/taks1/Calculator.java
+++ src/Tasks/taks1/Calculator.java
@@ -3,40 +3,74 @@ package Tasks.taks1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class Calculator {
     public static void main(String[] args) throws IOException {
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        final String SUM_SIGN = "+";
-        final String DIFFERENCE_SIGN = "-";
-        final String MULTIPLICATION_SIGN = "*";
-        final String RATIO_SIGN = "/";
         final String STOP_COMMAND = "stop";
         final String EXCEPTION_AND_ATTEMPTS = "You didn't enter a number. Try again! Attempts: ";
-
-        byte attempts;
+        final String PROMPT_TO_ENTER = "Enter the number. To exit - enter \"" + STOP_COMMAND + "\"";
+        final String DIVISION_BY_ZERO = "You cannot divide by zero! Attempts: ";
+        byte inputСounter = 1;
         String input = "";
-        double first;
-        double b;
+        double first = 0;
+        double second = 0;
+        boolean flag = true;
 
-        while (input.equalsIgnoreCase(STOP_COMMAND) != true) {
-            System.out.println("Enter the first number.");
-            attempts = 4;
+
+        while (flag) {
+            byte attempts = 3;
             while (attempts > 0) {
+                System.out.println(PROMPT_TO_ENTER);
                 input = reader.readLine();
-                if (input.equals("")) {
-                    attempts -= 1;
-                    System.out.println(EXCEPTION_AND_ATTEMPTS + attempts);
-                } else {
-                    try {
+                if (input.equalsIgnoreCase(STOP_COMMAND)) {
+                    flag = false;
+                    break;
+                }
+                try {
+                    if (inputСounter == 1) {
                         first = Double.parseDouble(input);
-                    } catch (NumberFormatException e) {
-                        attempts -= 1;
-                        System.out.println(EXCEPTION_AND_ATTEMPTS + attempts);
+                        inputСounter++;
+                        break;
                     }
+                    if (inputСounter == 2) {
+                            second = Double.parseDouble(input);
+                            inputСounter++;
+                            break;
+                    }
+                    if(inputСounter == 3){
+                        char command = input.charAt(0);
+                        inputСounter = 1;
+                        switch (command) {
+                            case '+':
+                                System.out.println(first + second);
+                                break;
+                            case '-':
+                                System.out.println(first - second);
+                                break;
+                            case '*':
+                                System.out.println(first * second);
+                                break;
+                            case '/':
+                                if(second == 0){
+                                    attempts--;
+                                    System.out.println(DIVISION_BY_ZERO + attempts);
+                                    break;
+                                }
+                                System.out.println(first / second);
+                                break;
+                            default:
+                                attempts--;
+                                System.out.println("You entered the wrong command! Attempts: " + attempts);
+                        }
+                    }
+                } catch (NumberFormatException e) {
+                    attempts--;
+                    System.out.println(EXCEPTION_AND_ATTEMPTS + attempts);
                 }
             }
-
         }
     }
 }
