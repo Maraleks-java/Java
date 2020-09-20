a648bb98f4d9eb619d93cbcd8a05aff0ce58e020
Aleksandr_Markov <aleksandr.markovgit@gmail.com>
2020-09-20 17:57:38 +0300

    Correct what you have when entering a command says "enter a number". And make the command input the second step.

--- src/Tasks/taks1/Calculator.java
+++ src/Tasks/taks1/Calculator.java
@@ -12,6 +12,7 @@ public class Calculator {
         final String STOP_COMMAND = "stop";
         final String EXCEPTION_AND_ATTEMPTS = "You didn't enter a number. Try again! Attempts: ";
         final String PROMPT_TO_ENTER = "Enter the number. To exit - enter \"" + STOP_COMMAND + "\"";
+        final String PROMPT_TO_COMMAND = "Enter the command [+ , -, *, /]. To exit - enter \"" + STOP_COMMAND + "\"";
         final String DIVISION_BY_ZERO = "You cannot divide by zero! Attempts: ";
         final String INCORRECT_COMMAND = "You entered the wrong command! Attempts: ";
         byte inputСounter = 1;
@@ -19,13 +20,19 @@ public class Calculator {
         double first = 0;
         double second = 0;
         boolean flag = true;
+        char command = 'd';
 
 
         while (flag) {
             byte attempts = 3;
             while (attempts > 0) {
-                System.out.println(PROMPT_TO_ENTER);
-                input = reader.readLine();
+                if(inputСounter == 2) {
+                    System.out.println(PROMPT_TO_COMMAND);
+                    input = reader.readLine();
+                } else if (inputСounter < 4) {
+                    System.out.println(PROMPT_TO_ENTER);
+                    input = reader.readLine();
+                }
                 if (input.equalsIgnoreCase(STOP_COMMAND)) {
                     flag = false;
                     break;
@@ -36,37 +43,50 @@ public class Calculator {
                         inputСounter++;
                         break;
                     }
-                    if (inputСounter == 2) {
-                            second = Double.parseDouble(input);
+
+                    if(inputСounter == 2 || inputСounter == 4){
+
+                        if(inputСounter == 2){
+                            command = input.charAt(0);
                             inputСounter++;
                             break;
-                    }
-                    if(inputСounter == 3){
-                        char command = input.charAt(0);
-                        inputСounter = 1;
-                        switch (command) {
-                            case '+':
-                                System.out.println(first + second);
-                                break;
-                            case '-':
-                                System.out.println(first - second);
-                                break;
-                            case '*':
-                                System.out.println(first * second);
-                                break;
-                            case '/':
-                                if(second == 0){
-                                    attempts--;
-                                    System.out.println(DIVISION_BY_ZERO + attempts);
+                        } else {
+                            switch (command) {
+                                case '+':
+                                    System.out.println(first + second);
+                                    inputСounter = 1;
                                     break;
-                                }
-                                System.out.println(first / second);
-                                break;
-                            default:
-                                attempts--;
-                                System.out.println(INCORRECT_COMMAND + attempts);
+                                case '-':
+                                    System.out.println(first - second);
+                                    inputСounter = 1;
+                                    break;
+                                case '*':
+                                    System.out.println(first * second);
+                                    inputСounter = 1;
+                                    break;
+                                case '/':
+                                    if (second == 0) {
+                                        attempts--;
+                                        System.out.println(DIVISION_BY_ZERO + attempts);
+                                        inputСounter = 1;
+                                        break;
+                                    }
+                                    System.out.println(first / second);
+                                    break;
+                                default:
+                                    attempts--;
+                                    System.out.println(INCORRECT_COMMAND + attempts);
+                            }
                         }
+
                     }
+
+                    if (inputСounter == 3) {
+                        second = Double.parseDouble(input);
+                        inputСounter++;
+                        break;
+                    }
+
                 } catch (NumberFormatException e) {
                     attempts--;
                     System.out.println(EXCEPTION_AND_ATTEMPTS + attempts);
